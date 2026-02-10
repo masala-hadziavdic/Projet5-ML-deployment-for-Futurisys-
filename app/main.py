687b3d2be0683a741506559471210b85a9b28a78
masala-hadziavdic <amela188@hotmail.com>
2026-02-10 21:42:06 +0100

    Fix model loading in CI

--- app/main.py
+++ app/main.py
@@ -2,15 +2,21 @@ from fastapi import FastAPI
 import joblib
 import pandas as pd
 from app.employee_models import EmployeeData
+import os
 
 app = FastAPI(title="ML Model API")
 
 # Charger le modèle
-model = joblib.load("app/model/model.pkl")
+if os.getenv("CI") == "true":
+    model = None
+else:
+    model = joblib.load("app/model/model.pkl")
+
 
 @app.get("/")
 def home():
     return {"message": "API is running"}
+
 
 @app.post("/predict")
 def predict(data: EmployeeData):
@@ -19,21 +25,25 @@ def predict(data: EmployeeData):
         input_dict = data.model_dump()
         df = pd.DataFrame([input_dict])
 
-        # Prédiction
-        prediction = model.predict(df)[0]  # récupère 0 ou 1
-        proba = model.predict_proba(df)[0]  # [prob_stay, prob_quit] si classifier binaire
+        # Vérifier que le modèle existe
+        if model is None:
+            return {"error": "Model not loaded"}
 
-        # Transformer en sortie lisible
+        # Prédiction
+        prediction = model.predict(df)[0]
+        proba = model.predict_proba(df)[0]
+
         result = {
-            "prediction": "Oui" if prediction == 1 else "Non",  # 1 = quitte, 0 = reste
-            "probability_quit": float(proba[1]),  # probabilité de quitter
-            "probability_stay": float(proba[0])   # probabilité de rester
+            "prediction": "Oui" if prediction == 1 else "Non",
+            "probability_quit": float(proba[1]),
+            "probability_stay": float(proba[0])
         }
 
         return result
 
     except Exception as e:
         return {"error": str(e)}
+
 
 
 
