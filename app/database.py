f45c61297804101de8ef97a78fc5fe75cdeea49e
masala-hadziavdic <amela188@hotmail.com>
2026-04-05 17:16:36 +0200

    Project structure + database config + gitignore

--- app/database.py
+++ app/database.py
@@ -1,14 +1,19 @@
+from dotenv import load_dotenv
+import os
 import psycopg2
 from contextlib import contextmanager
+
+# Charger le fichier .env
+load_dotenv()
 
 @contextmanager
 def get_connection():
     conn = psycopg2.connect(
-        dbname="ml_database",
-        user="postgres",
-        password="0000",
-        host="localhost",
-        port="5434"
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
     )
     try:
         yield conn
@@ -19,4 +24,3 @@ def get_connection():
         raise e
     finally:
         conn.close()
-
